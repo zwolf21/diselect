620ddba3b7884f9cb83df5aca783732b2e64cb41
zwolf21 <pbr112@naver.com>
2022-05-31 19:22:45 +0900

    upload readme

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
 	name='diselect',
-	version='0.0.1',
+	version='0.0.2',
 	license='MIT',
 	description='smart and convenient value selector in complex container nested with dict and list(from json data)',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
 	author = 'HS Moon',
 	author_email = 'pbr112@naver.com',
 	py_modules = ['diselect'],
@@ -19,4 +23,5 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
+	python_requires=">=3.8"
 )

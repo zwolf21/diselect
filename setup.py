130e0d1c155ccb145b6bec602be266390e3067a3
zwolf21 <pbr112@naver.com>
2022-06-08 13:14:36 +0900

    setup.py keyword error fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
 	name='diselect',
-	version='1.0.1',
+	version='1.0.2',
 	license='MIT',
 	description='smart and convenient dict flatten library for complex container nested with dict and list',
 	long_description=long_description,
@@ -15,7 +15,7 @@ setup(
 	author_email = 'pbr112@naver.com',
 	py_modules = ['diselect'],
 	install_requires=[],
-	keywords='crawlite',
+	keywords=['diselect', 'dict select', 'flatten', 'dict flatten', 'dict in list', 'list of dict'],
 	url='https://github.com/zwolf21/diselect',
 	packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 	classifiers=[

8a7a068bfbec4d69590e2a5968e460e994ee5c3d
zwolf21 <pbr112@naver.com>
2022-07-27 23:07:55 +0900

    drop support <3.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
 	name='diselect',
-	version='1.0.9',
+	version='1.0.10',
 	license='MIT',
 	description='smart and convenient dict flatten library for complex container nested with dict and list',
 	long_description=long_description,
@@ -19,9 +19,8 @@ setup(
 	url='https://github.com/zwolf21/diselect',
 	packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 	classifiers=[
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
-	python_requires=">=3.8"
+	python_requires=">=3.9"
 )
